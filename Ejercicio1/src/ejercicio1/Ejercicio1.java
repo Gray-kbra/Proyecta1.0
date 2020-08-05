c47b42ef36cd3c355bb115a42daf753cb6d5a204
Graysi <Graysi@192.168.1.141>
2020-08-05 11:21:47 -0500

    Primera modificación_a, por favor revisarlo

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -17,6 +17,7 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         // TODO code application logic here
         int x=2;
+        System.out.println("Valor:"+1);
     }
     
 }
