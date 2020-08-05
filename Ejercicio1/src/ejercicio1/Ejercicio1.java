874ff9d0cef1258b4cd0ad86eba0be17e7b6d14c
Gray-kbra <69255277+Gray-kbra@users.noreply.github.com>
2020-08-05 11:29:46 -0500

    Update Ejercicio1.java
    Ya modifique el proyecto. Aumente la variable precio

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -17,7 +17,11 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         // TODO code application logic here
         int x=2;
+        double Y=0;
         System.out.println("Valor:"+1);
+        System.out.println("Valor:"+2);
+        System.out.println("Valor:"+3);
+        System.out.println("Valor:"+4);
     }
     
 }
